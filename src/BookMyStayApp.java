4f777761c9bb4e1bbbb74778d730c6b949ecaf44
anshuankur <aa2558@srmist.edu.in>
2026-03-18 09:29:27 +0530

    UC3: Centralized Room Inventory Management

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,26 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class RoomInventory {
+
+    private Map<String, Integer> roomAvailability;
+
+    public RoomInventory() {
+        roomAvailability = new HashMap<>();
+        initializeInventory();
+    }
+
+    private void initializeInventory() {
+        roomAvailability.put("Single", 5);
+        roomAvailability.put("Double", 3);
+        roomAvailability.put("Suite", 2);
+    }
+
+    public Map<String, Integer> getRoomAvailability() {
+        return roomAvailability;
+    }
+
+    public void updateAvailability(String roomType, int count) {
+        roomAvailability.put(roomType, count);
+    }
+}
+
 public class BookMyStayApp {
+
     public static void main(String[] args) {
+
+        RoomInventory inventory = new RoomInventory();
+
+        Map<String, Integer> availability = inventory.getRoomAvailability();
 
         System.out.println("Hotel Room Inventory Status\n");
 
         System.out.println("Single Room:");
-        System.out.println("Beds: 1");
-        System.out.println("Size: 250 sqft");
-        System.out.println("Price per night: 1500.0");
-        System.out.println("Available Rooms: 5\n");
+        System.out.println("Available Rooms: " + availability.get("Single") + "\n");
 
         System.out.println("Double Room:");
-        System.out.println("Beds: 2");
-        System.out.println("Size: 400 sqft");
-        System.out.println("Price per night: 2500.0");
-        System.out.println("Available Rooms: 3\n");
+        System.out.println("Available Rooms: " + availability.get("Double") + "\n");
 
         System.out.println("Suite Room:");
-        System.out.println("Beds: 3");
-        System.out.println("Size: 750 sqft");
-        System.out.println("Price per night: 5000.0");
-        System.out.println("Available Rooms: 2");
-
+        System.out.println("Available Rooms: " + availability.get("Suite"));
     }
-
 }
